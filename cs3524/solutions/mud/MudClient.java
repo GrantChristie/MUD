b10b136598f161b26f673de6228887046728a51d
GrantChristie <grantchristie0@gmail.com>
2017-03-08 12:43:06 +0000

    prevent user from picking up themself

--- cs3524/solutions/mud/MudClient.java
+++ cs3524/solutions/mud/MudClient.java
@@ -6,7 +6,9 @@ public class MudClient{
       System.err.println("Usage: \njava MudClient <host> <port>");
       return;
     }
+
     String registryURL = "rmi://" + args[0] + ":" + args[1] + "/MudService";
+
     try{
       MudServerInterface server = (MudServerInterface)java.rmi.Naming.lookup(registryURL);
       String user = System.console().readLine("Please enter your username: ");
@@ -15,15 +17,25 @@ public class MudClient{
       System.out.println("\nTo get instructions, type 'help'.");
       while (!input.equals("exit")){
           System.out.println(server.status(currentLocation));
-          input = System.console().readLine("Do Something: ");
+          input = System.console().readLine("What would you like to do? ");
           currentLocation = server.move(currentLocation, input, user);
+
         if (input.equals("help")){
-          System.out.println("To exit the game, type 'exit'.");
+          System.out.println("\nTo exit the game, type 'exit'.");
           System.out.println("To move, type the direction you wish to move in. 'north', 'south', 'east', 'west'.");
-        }else if(input.contains("pickup")){
-          input = input.replace("pickup ", "");
-          server.pickup(currentLocation, input);
+          System.out.println("To pickup an item, type 'pickup' and the item you wish to pickup.");
         }
+
+        else if(input.contains("pickup")){
+          input = input.replace("pickup ", "");
+          if (input.equals(user)){
+            System.out.println("You cannot pickup yourself");
+          }
+          else{
+            server.pickup(currentLocation, input);
+          }
+        }
+
       }
     }catch(Exception e) {
         System.err.println(e.getMessage());
