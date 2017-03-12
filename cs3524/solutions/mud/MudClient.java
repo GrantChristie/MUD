022b61f97ed7ba3e0f217c704e2ef64d6647c47e
GrantChristie <grantchristie0@gmail.com>
2017-03-12 15:18:49 +0000

    comments

--- cs3524/solutions/mud/MudClient.java
+++ cs3524/solutions/mud/MudClient.java
@@ -15,17 +15,23 @@ public class MudClient{
       String currentLocation = server.setup(user);
       String input = "";
       System.out.println("\nTo get instructions, type 'help'.");
+
+      //Main game loop, ends when user inputs "exit
       while (!input.equals("exit")){
           System.out.println(server.status(currentLocation));
           input = System.console().readLine("What would you like to do? ");
-          currentLocation = server.move(currentLocation, input, user);
 
+        //if user inputs help, game instructions are printed
         if (input.equals("help")){
           System.out.println("\nTo exit the game, type 'exit'.");
           System.out.println("To move, type the direction you wish to move in. 'north', 'south', 'east', 'west'.");
           System.out.println("To pickup an item, type 'pickup' and the item you wish to pickup.");
         }
-
+        //if user inputs one of 4 directions, the value of currentLocation is updated.
+        else if (input.equalsIgnoreCase("north") || input.equalsIgnoreCase("east") || input.equalsIgnoreCase("south") || input.equalsIgnoreCase("west")){
+           currentLocation = server.move(currentLocation, input.toLowerCase(), user);
+        }
+        //if the user inputs pickup and an item, that item is removed from the currentLocation.
         else if(input.contains("pickup")){
           input = input.replace("pickup ", "");
           if (input.equals(user)){
@@ -34,6 +40,10 @@ public class MudClient{
           else{
             server.pickup(currentLocation, input);
           }
+        }
+        //if any other input is received, message is printed informing the user.
+        else {
+          System.out.println("\nInvalid Action");
         }
 
       }
--- cs3524/solutions/mud/MudServerImpl.java
+++ cs3524/solutions/mud/MudServerImpl.java
@@ -5,19 +5,23 @@ public class MudServerImpl implements MudServerInterface {
 
   public MudServerImpl(){ }
 
+  //Adds the user's name to the start location to enable the user to start playing.
   public String setup(String name){
     m.addThing(m.startLocation(), name);
     return m.startLocation();
   }
 
+  //Returns information about the requested location.
   public String status(String location){
     return m.locationInfo(location);
   }
 
+  //Moves the player given a location, direction and thing.
   public String move(String loc, String dir, String thing){
     return m.moveThing(loc, dir, thing);
   }
 
+  //Removes the item being picked up from it's current location.
   public String pickup(String loc, String thing){
     m.delThing(loc, thing);
     return thing;
